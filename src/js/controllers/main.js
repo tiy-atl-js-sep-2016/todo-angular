70bd36817aae54d6b4140f627942a5a7f2dada75
Brit Butler <redline6561@gmail.com>
2016-11-14 11:32:34 -0500

    Clean out some code not needed for the final app.

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -1,21 +1,10 @@
 function MainController ($scope) {
   $scope.tasks = [];
-  $scope.example = {
-    name: 'jurgen',
-    breed: 'mixed',
-    cuteness: '100 emoji'
-  };
-  $scope.buttonTitle = "this is a button";
-
-  $scope.changeTitle = function () {
-    $scope.buttonTitle = "Buttons are dumb";
-  };
 
   $scope.addTask = function (title) {
     let newTask = { title: title, id: $scope.tasks.length + 1 };
     console.log(newTask);
     $scope.tasks.push(newTask);
-
     $scope.message = '';
   };
 
