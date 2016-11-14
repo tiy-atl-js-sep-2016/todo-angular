e123481b3db4041ec2834117ead79200d216daaf
Brit Butler <redline6561@gmail.com>
2016-11-14 11:31:41 -0500

    Finish our basic todo list app.

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -1,7 +1,24 @@
 function MainController ($scope) {
-  console.log("hello from main.js");
-  console.log($scope);
-  $scope.message = "Hello from scope";
+  $scope.tasks = [];
+  $scope.example = {
+    name: 'jurgen',
+    breed: 'mixed',
+    cuteness: '100 emoji'
+  };
+  $scope.buttonTitle = "this is a button";
+
+  $scope.changeTitle = function () {
+    $scope.buttonTitle = "Buttons are dumb";
+  };
+
+  $scope.addTask = function (title) {
+    let newTask = { title: title, id: $scope.tasks.length + 1 };
+    console.log(newTask);
+    $scope.tasks.push(newTask);
+
+    $scope.message = '';
+  };
+
 };
 
 MainController.$inject = ['$scope'];
